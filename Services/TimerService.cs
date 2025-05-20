c38b961e9a4328511393323b23c25bc26a5f3016
Behzod <behzodsfary@gmail.com>
2025-05-20 22:55:35 +0500

    Ortiqcha kodlardan tozalandi

--- Services/TimerService.cs
+++ Services/TimerService.cs
@@ -4,8 +4,6 @@ using Timer = System.Timers.Timer;
 using TimerTestApp.Data;
 using TimerTestApp.Models;
 using TimerTestApp.Shared;
-using Microsoft.Extensions.DependencyInjection;
-using ActualLab.Fusion;
 
 namespace TimerTestApp.Services;
 
